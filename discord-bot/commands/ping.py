29430757a398d4a168a1a44dfcc5b3f531bb01f1
Ubuntu <anhsp@ProgramAI.n5ubkhgtko2exgixkz0md0wnad.jnbx.internal.cloudapp.net>
2025-05-05 01:09:56 +0000

    Add commands folder for Discord bot

---
+++ discord-bot/commands/ping.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="ping")
+    async def ping(self, ctx):
+        await ctx.send("Pong! 🏓")
+
+def setup(bot):
+    bot.add_cog(Ping(bot))
